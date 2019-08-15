5b99e1f6d1f0481230744aa9c4d2126ebec48c45
彭羽 <wb.pengyu@mesg.corp.netease.com>
2019-08-15 19:41:04 +0800

    add：增加超时

--- src/main/java/com/netease/seckill/controller/SecKillController.java
+++ src/main/java/com/netease/seckill/controller/SecKillController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netease.seckill.cache.RedisService;
 import com.netease.seckill.service.StockService;
 
 /**
