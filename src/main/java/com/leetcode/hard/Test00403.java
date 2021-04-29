f3d74fb878db669bc80e43358113d246dfb42845
pengyu <31266246+yannisPeng@users.noreply.github.com>
2021-04-29 09:34:05 +0800

    add：动态规划

--- src/main/java/com/leetcode/hard/Test00403.java
+++ src/main/java/com/leetcode/hard/Test00403.java
@@ -2,7 +2,7 @@ package com.leetcode.hard;
 
 
 /**
- * 青蛙过河
+ * 青蛙过河-动态规划
  *
  * 一只青蛙想要过河。 假定河流被等分为若干个单元格，并且在每一个单元格内都有可能放有一块石子（也有可能没有）。 青蛙可以跳上石子，但是不可以跳入水中。
  *
@@ -36,7 +36,33 @@ package com.leetcode.hard;
 public class Test00403 {
 
     public boolean canCross(int[] stones) {
-        
+         /*
+         动态规划
+         dp[i][k] 表示能否由前面的某一个石头 j 通过跳 k 步到达当前这个石头 i ，这个 j 的范围是 [1, i - 1]
+         当然，这个 k 步是 i 石头 和 j 石头之间的距离
+         那么对于 j 石头来说，跳到 j 石头的上一个石头的步数就必须是这个 k - 1 || k || k + 1
+         由此可得状态转移方程：dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1]
+        */
+        int len = stones.length;
+
+        if (stones[1] != 1) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[len][len + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                int k = stones[i] - stones[j];
+                if (k <= j + 1) {
+                    dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+                    //提前结束循环直接返回结果
+                    if (i == len - 1 && dp[i][k]) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 }
