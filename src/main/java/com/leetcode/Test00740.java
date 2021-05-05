44689a1aa6d98d42ef84a3e8c7e345e1c06712f8
pengyu <31266246+yannisPeng@users.noreply.github.com>
2021-05-05 19:27:24 +0800

    add：740

---
+++ src/main/java/com/leetcode/Test00740.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+/**
+ * Description: 删除并获得点数
+ *
+ * @author 10014994
+ * @since 2021/5/5
+ */
+public class Test00740 {
+
+    public int deleteAndEarn(int[] nums) {
+        
+        return 0;
+    }
+
+}
