9651b33525ccbb78851f42e01d16b8b197589083
彭羽 <31266246+yannisPeng@users.noreply.github.com>
2021-05-17 10:40:33 +0800

    add：二叉树

---
+++ src/main/java/com/leetcode/bintree/Test00993.java
@@ -0,0 +1,51 @@
+package com.leetcode.bintree;
+
+/**
+ * @author yannisPeng@gmail.com
+ * @date 2021/5/17 9:35
+ * <p>
+ * 二叉树的堂兄弟节点
+ * <p>
+ * 在二叉树中，根节点位于深度 0 处，每个深度为 k 的节点的子节点位于深度 k+1 处。
+ * 如果二叉树的两个节点深度相同，但 父节点不同 ，则它们是一对堂兄弟节点。
+ * 我们给出了具有唯一值的二叉树的根节点 root ，以及树中两个不同节点的值 x 和 y 。
+ * 只有与值 x 和 y 对应的节点是堂兄弟节点时，才返回 true 。否则，返回 false。
+ * <p>
+ * 示例 1：
+ * 输入：root = [1,2,3,4], x = 4, y = 3
+ * 输出：false
+ * <p>
+ * 示例 2：
+ * 输入：root = [1,2,3,null,4,null,5], x = 5, y = 4
+ * 输出：true
+ * <p>
+ * 示例 3：
+ * 输入：root = [1,2,3,null,4], x = 2, y = 3
+ * 输出：false
+ * <p>
+ */
+public class Test00993 {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+        return false;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
