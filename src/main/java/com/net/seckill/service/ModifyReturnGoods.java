adb93b93c328e48b429cb8605fd458930dfc1ab7
yannisPeng <yannisPeng@gmail.com>
2022-05-12 06:33:21 +0800

    1

--- src/main/java/com/net/seckill/service/ModifyReturnGoods.java
+++ src/main/java/com/net/seckill/service/ModifyReturnGoods.java
@@ -1,9 +1,18 @@
 package com.net.seckill.service;
 
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModifyReturnGoods {
 
     public static void main(String[] args) {
-        System.out.println("skucode");
+        if (CollectionUtils.isEmpty(new ArrayList<>()) && CollectionUtils.isEmpty(new ArrayList<>())) {
+
+        }
+
+        List<String> returnExchangeGoodsSkc = new ArrayList<>();
         System.out.println("skucode");
     }
 
